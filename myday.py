a1d50c73cebf06e0123a360d02aa021707f83a68
Marius Ndini <mariusndini@yahoo.com>
2022-10-18 10:07:50 -0400

    Update myday.py

--- myday.py
+++ myday.py
@@ -132,60 +132,62 @@ def train():
     st.sidebar.write('Here You will name your model and the model will be trained on Snowflake')
     st.markdown("# Train Model 🤖")
 
+    model_name = st.text_input('Name your model', '')
+
     if st.button('Train My Model'):
-        grade_SQL = f""" call SH_MARIUS.PIPELINE_TRAIN.TRAIN_MODEL('DefaultModelName'); """
+        grade_SQL = f""" call SH_MARIUS.PIPELINE_TRAIN.TRAIN_MODEL('{model_name}'); """
         st.write( f"""Your Model: {run_query(grade_SQL)[0][0] }""")
 
-        code = '''
-        CREATE OR REPLACE PROCEDURE SH_MARIUS.PIPELINE_TRAIN.TRAIN_MODEL(MODEL_NAME STRING)
-            RETURNS STRING
-            LANGUAGE PYTHON
-            RUNTIME_VERSION = '3.8'
-            PACKAGES = ('snowflake-snowpark-python', 'scikit-learn')
-            HANDLER = 'main'
-            AS
-            $$
+    code = '''
+CREATE OR REPLACE PROCEDURE SH_MARIUS.PIPELINE_TRAIN.TRAIN_MODEL(MODEL_NAME STRING)
+    RETURNS STRING
+    LANGUAGE PYTHON
+    RUNTIME_VERSION = '3.8'
+    PACKAGES = ('snowflake-snowpark-python', 'scikit-learn')
+    HANDLER = 'main'
+    AS
+    $$
 
-            import snowflake.snowpark as snowpark
-            from snowflake.snowpark import Session
-            from snowflake.snowpark.functions import udf
-            from snowflake.snowpark.functions import col
-            from snowflake.snowpark import functions as F
-            from snowflake.snowpark.types import *
-            from sklearn.neural_network import MLPClassifier
+    import snowflake.snowpark as snowpark
+    from snowflake.snowpark import Session
+    from snowflake.snowpark.functions import udf
+    from snowflake.snowpark.functions import col
+    from snowflake.snowpark import functions as F
+    from snowflake.snowpark.types import *
+    from sklearn.neural_network import MLPClassifier
 
-            def main(session: snowpark.Session, MODEL_NAME):
-                MODELNAME = MODEL_NAME #MODEL NAME COMING FROM THE S.PROC DEF
+    def main(session: snowpark.Session, MODEL_NAME):
+        MODELNAME = MODEL_NAME #MODEL NAME COMING FROM THE S.PROC DEF
 
-                mlpc = MLPClassifier(hidden_layer_sizes = (10, 5), max_iter=2000);
-                
-                x = session.sql(f"""SELECT CAL_DIFF, CARBS, FAT, PROTEIN, CHOL, SALT, SUGAR 
-                                    FROM SH_MARIUS.pipeline_train.GRADED_DIET
-                                    WHERE DIET_NAME = '{MODELNAME}';""").collect()
-                y = session.sql(F"""SELECT GRADE 
-                                    FROM SH_MARIUS.pipeline_train.GRADED_DIET
-                                    WHERE DIET_NAME = '{MODELNAME}';""").collect()
+        mlpc = MLPClassifier(hidden_layer_sizes = (10, 5), max_iter=2000);
+        
+        x = session.sql(f"""SELECT CAL_DIFF, CARBS, FAT, PROTEIN, CHOL, SALT, SUGAR 
+                            FROM SH_MARIUS.pipeline_train.GRADED_DIET
+                            WHERE DIET_NAME = '{MODELNAME}';""").collect()
+        y = session.sql(F"""SELECT GRADE 
+                            FROM SH_MARIUS.pipeline_train.GRADED_DIET
+                            WHERE DIET_NAME = '{MODELNAME}';""").collect()
 
-                # Model Training 
-                mlpc.fit(x, y) 
+        # Model Training 
+        mlpc.fit(x, y) 
 
-                @udf(name= f"""SH_MARIUS.pipeline_train.PREDICT_{MODELNAME}""", 
-                    return_type=IntegerType(), 
-                    packages=["scikit-learn"],
-                    is_permanent=True, replace=True, 
-                    stage_location="@SH_MARIUS.GRADES.HEALTH_STAGE",
-                    input_types=[FloatType(), FloatType(),FloatType(), FloatType(), FloatType(), FloatType(), FloatType()])
-                
-                def PREDICT_DIET(calAte, carbs, fat, protein, chol, salt, sugar):
-                    return mlpc.predict( [[calAte, carbs, fat, protein, chol, salt, sugar]] )[0]
+        @udf(name= f"""SH_MARIUS.pipeline_train.PREDICT_{MODELNAME}""", 
+            return_type=IntegerType(), 
+            packages=["scikit-learn"],
+            is_permanent=True, replace=True, 
+            stage_location="@SH_MARIUS.GRADES.HEALTH_STAGE",
+            input_types=[FloatType(), FloatType(),FloatType(), FloatType(), FloatType(), FloatType(), FloatType()])
+        
+        def PREDICT_DIET(calAte, carbs, fat, protein, chol, salt, sugar):
+            return mlpc.predict( [[calAte, carbs, fat, protein, chol, salt, sugar]] )[0]
 
-                session.sql(f"""insert into SH_MARIUS.pipeline_train.model_list values ('{MODELNAME}');""").collect()
-                return f"""Model {MODELNAME} Trained & Ready to Use"""
+        session.sql(f"""insert into SH_MARIUS.pipeline_train.model_list values ('{MODELNAME}');""").collect()
+        return f"""Model {MODELNAME} Trained & Ready to Use"""
 
-            $$
+    $$
         
         '''
-        st.code(code, language='python')
+    st.code(code, language='python')
 
 
 # ----------------------------------------------------------------------------------
@@ -199,7 +201,7 @@ def test():
     st.sidebar.markdown("# Test Models 🧠")
     st.markdown("# Test Models 🧠")
 
-    save_SQL = f""" select * from sh_marius.pipeline_train.model_list; """
+    save_SQL = f""" select distinct * from sh_marius.pipeline_train.model_list; """
     trained_models = run_query(save_SQL)
 
     option = st.selectbox('Please Select a Trained Model Below', [i[0] for i in trained_models])
